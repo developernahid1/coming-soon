bbeeeb45a05e6038acee054316fb4ae5422c45e6
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-03-06 16:30:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const launchDate = new Date("March 15, 2024 00:00:00").getTime();
+const intvl = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = launchDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("days").innerHTML = days;
+}, 1000);
